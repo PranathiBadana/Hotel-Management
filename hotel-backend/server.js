aeea73a4381e73f9ec97dfc4175b3f15bacaec3d
PranathiBadana <pranathibadana414@gmail.com>
2026-06-29 01:46:46 +0530

    fix cors for production

--- hotel-backend/server.js
+++ hotel-backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://hotel-management-dun-ten.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
